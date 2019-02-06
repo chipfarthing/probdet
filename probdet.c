e9a01e370c0d50c92f2627872af6bddc5dfb97e8
chipfarthing <47392027+chipfarthing@users.noreply.github.com>
2019-02-06 10:32:36 -0500

    Add files via upload

---
+++ probdet.c
@@ -0,0 +1,47 @@
+#include "math.h"
+#include "stdio.h"
+
+float probdet(float N, float Yb, float K, float xdB)
+{
+  float x, T, a, Pd, f, term, k, lastterm, e, pi;
+  float res, ims, mags, angs, mags1, angs1, magden, angden, reden, imden;
+  
+  e=1e-6;
+  pi=3.14159265358979;
+  x=pow(10,xdB/10);
+  if((N==1) && (K==1))
+     Pd=exp(-Yb/(1+x));
+  if((N==1) && (K==2))
+     Pd=(1-(pow(1/(1+x/2),2)-1/(1+x/2))*Yb)*exp(-Yb/(1+x/2));
+  if(((N==1) && (K!=1) && (K!=2)) || (N!=1))
+  {
+  T=2*Yb;
+  a=-log(e)/T;
+  f=exp(a*Yb)/T*1/pow(a+1,N)/pow(1+a/(1+a)*N*x/K,K)/a;
+  term=f;
+  k=0;
+  lastterm=1e9;
+  while(fabs(term)>e || fabs(lastterm)>e)
+  {
+     k=k+1;
+     lastterm=term;
+     res=a;
+     ims=k*pi/T;
+     mags=sqrt(pow(res,2)+pow(ims,2));
+     angs=atan2(ims,res);
+     mags1=sqrt(pow(1+res,2)+pow(ims,2));
+     angs1=atan2(ims,1+res);
+     magden=mags/mags1*N*x/K;
+     angden=angs-angs1;
+     reden=1+magden*cos(angden);
+     imden=magden*sin(angden);
+     magden=pow(sqrt(pow(reden,2)+pow(imden,2)),-K)/mags/pow(mags1,N);
+     angden=-atan2(imden,reden)*K-angs-angs1*N;
+     term=2*exp(a*Yb)/T*magden*cos(angden)*cos(k*pi/T*Yb);
+     f=f+term;
+   };
+   Pd=1-f; 
+  }
+  return Pd;
+}
+
---
+++ probdet.py
@@ -0,0 +1,69 @@
+from ctypes import *
+
+from scipy.stats import gamma
+
+import numpy as np
+
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, Slider, Select, Range1d, TextInput, Spacer
+from bokeh.plotting import Figure, curdoc
+
+pd=cdll.LoadLibrary("./libprobdet.so")
+pd.probdet.argtypes=[c_float, c_float, c_float, c_float]
+pd.probdet.restype=c_float
+
+pfa=TextInput(title='False Alarm Probability', value='1e-6')
+slider1 = Slider(start=1, end=10, value=1, step=1, title="Number of Pulses")
+slider2 = Slider(start=.5, end=10, value=1, step=0.5, title="Target Gamma K parameter")
+case=Select(title="Swerling Case", value="Case 2", options=["Case 1", "Case 2", "Case 3", "Case 4", "Non-fluctuating"])
+space=Spacer(height=30, width=400)
+
+PFA=float(pfa.value)
+N=slider1.value
+Yb=gamma.ppf(1-PFA,N)
+K=1*N;
+
+xdBseries=np.array([x*0.1 for x in range(0, 501)])
+xdBseries=xdBseries-10
+
+vprobdet=np.vectorize(pd.probdet)
+
+PDseries=vprobdet(N,Yb,K,xdBseries)
+
+source=ColumnDataSource(data=dict(snr=xdBseries, pd=PDseries))
+
+plot = Figure(plot_width=435, plot_height=400)
+plot.line('snr','pd',source=source)
+
+plot.x_range=Range1d(-10 , 40)
+
+def update(attrname, old, new):
+
+    PFA=float(pfa.value)
+    N=slider1.value
+    Yb=gamma.ppf(1-PFA,N)
+    if case.value=="Case 1":
+        K=1
+    elif case.value=="Case 2":
+        K=N
+    elif case.value=="Case 3":
+        K=2
+    elif case.value=="Case 4":
+        K=2*N
+    else:
+        K=1000
+    newPDseries=vprobdet(N,Yb,K,xdBseries)
+    newdata=dict(snr=xdBseries, pd=newPDseries)
+    source.data = newdata
+
+controls=[slider1, case, pfa]
+
+for control in controls:
+    control.on_change('value', update)
+
+layout = column(pfa, case, slider1, space, plot)
+
+curdoc().add_root(layout)
+
+
+
